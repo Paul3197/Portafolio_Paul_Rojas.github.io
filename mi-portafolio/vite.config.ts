286a9284d0583b147aedccff718dc927dbdabab9
Paul3197 <paulrojas123231@gmail.com>
2025-06-13 08:53:50 -0500

    Cambio vite

--- mi-portafolio/vite.config.ts
+++ mi-portafolio/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portafolio-paul/',
+  base: "/portafolio-paul/",
   plugins: [react(), tailwindcss()]
 })
