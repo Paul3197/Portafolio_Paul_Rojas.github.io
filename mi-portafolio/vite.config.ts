51eb57c091e4aed15c9a512eff2f9b35d4ed7293
Paul3197 <paulrojas123231@gmail.com>
2025-06-13 08:58:49 -0500

    cambio raiz

--- mi-portafolio/vite.config.ts
+++ mi-portafolio/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/portafolio-paul/",
+  base: '/',
   plugins: [react(), tailwindcss()]
 })
