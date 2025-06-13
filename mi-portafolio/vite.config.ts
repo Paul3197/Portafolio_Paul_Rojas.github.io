a99beda509e459ec029e1d4c7440a4d9794515b9
Paul3197 <paulrojas123231@gmail.com>
2025-06-12 23:46:23 -0500

    alpha3.0

--- mi-portafolio/vite.config.ts
+++ mi-portafolio/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // 👈 AÑADE ESTA LÍNEA
+  base: '/portafolio-paul/',
   plugins: [react(), tailwindcss()]
 })
